95ebefca1e84eebb3bb122619a447befce2a9393
Brian Grapes <bgrapes@gmail.com>
2022-02-18 11:06:20 -0800

    feat(advice-generator): update advice on click

--- advice-generator-app/src/index.js
+++ advice-generator-app/src/index.js
@@ -1,9 +1,14 @@
-const button = document.querySelector("button");
+const button = document.getElementById("new-advice-button");
+const adviceId = document.getElementById("advice-id");
+const adviceContent = document.getElementById("advice-content");
 
 function getAdvice() {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      adviceId.innerHTML = data.slip.id;
+      adviceContent.innerHTML = data.slip.advice;
+    });
 }
 
 button.addEventListener("click", getAdvice);
