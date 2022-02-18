9470c098bef0eec1ae2ec8b3e9cdce1b39569151
Brian Grapes <bgrapes@gmail.com>
2022-02-18 12:32:26 -0800

    feat(advice-generator): finish styling

--- advice-generator-app/src/index.js
+++ advice-generator-app/src/index.js
@@ -1,3 +1,6 @@
+import "animate.css";
+
+const container = document.querySelector(".advice-generator");
 const button = document.getElementById("new-advice-button");
 const adviceId = document.getElementById("advice-id");
 const adviceContent = document.getElementById("advice-content");
@@ -6,9 +9,17 @@ function getAdvice() {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((data) => {
+      animate();
       adviceId.innerHTML = data.slip.id;
       adviceContent.innerHTML = data.slip.advice;
     });
+}
+
+function animate() {
+  container.classList.add("animate__animated", "animate__fadeIn");
+  container.addEventListener("animationend", () => {
+    container.classList.remove("animate__animated", "animate__fadeIn");
+  });
 }
 
 button.addEventListener("click", getAdvice);
