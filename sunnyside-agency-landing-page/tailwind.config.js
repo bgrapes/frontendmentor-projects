7eff95d4f2e1c46794d877e6fa2f4528c63e0c6e
Brian Grapes <bgrapes@gmail.com>
2022-02-22 17:32:01 -0800

    feat(sunnyside-lp): add first section

--- sunnyside-agency-landing-page/tailwind.config.js
+++ sunnyside-agency-landing-page/tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     container: false,
   },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "header-desktop": "url('./images/desktop/image-header.jpg')",
+        "header-mobile": "url('./images/mobile/image-header.jpg')",
+      },
+      colors: {
+        "soft-red": "hsl(7, 99%, 70%)",
+        yellow: "hsl(51, 100%, 49%)",
+        "dark-desaturated-cyan": "hsl(167, 40%, 24%)",
+        "dark-blue": "hsl(198, 62%, 26%)",
+        "dark-moderate-cyan": "hsl(168, 34%, 41%)",
+        "very-dark-desaturated-blue": "hsl(212, 27%, 19%)", // button text
+        "very-dark-grayish-blue": "hsl(213, 9%, 39%)",
+        "dark-grayish-blue": "hsl(232, 10%, 55%)",
+        "grayish-blue": "hsl(210, 4%, 67%)",
+        white: "hsl(0, 0%, 100%)",
+        "light-gray": "#feffff",
+      },
+      fontFamily: {
+        body: ["Barlow", "Helvetica", "Arial", "sans-serif"],
+        display: ["Fraunces", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [
     function ({ addComponents }) {
