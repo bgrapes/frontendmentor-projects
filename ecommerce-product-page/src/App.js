73b9f32b40261a11aa14207dc6051693591e3d65
Brian Grapes <bgrapes@gmail.com>
2022-03-08 11:13:31 -0800

    feat(ecommerce): initialize project

---
+++ ecommerce-product-page/src/App.js
@@ -0,0 +1,16 @@
+export function App() {
+  return (
+    <div>
+      <main className="container">
+        <h1>eCommerce product page</h1>
+      </main>
+      <footer class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/bgrapes">Brian Grapes</a>.
+      </footer>
+    </div>
+  );
+}
