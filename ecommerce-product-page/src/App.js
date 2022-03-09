db0b7d0b43e4d1139100fd669820654b3b85cf1b
Brian Grapes <bgrapes@gmail.com>
2022-03-09 10:14:52 -0800

    feat(styles): use createGlobalStyle

--- ecommerce-product-page/src/App.js
+++ ecommerce-product-page/src/App.js
@@ -1,9 +1,69 @@
+import Nav from "./components/Nav";
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(--white);
+    color: var(--very-dark-blue);
+    font-family: "Manrope", Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  
+  a {
+    text-decoration: none;
+  }
+
+  /*
+  * Containers
+  */
+  .container {
+    display: flex;
+    min-height: 100vh;
+  }
+
+  /*
+  * Footer
+  */
+  .attribution {
+    color: var(--very-dark-blue);
+    font-size: 11px;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+
+  .attribution a {
+    color: var(--very-dark-blue);
+  }
+
+  .attribution a:hover {
+    color: var(--black);
+  }
+`;
+
 export function App() {
   return (
     <div>
-      <main className="container">
-        <h1>eCommerce product page</h1>
-      </main>
+      <GlobalStyle />
+      <div className="container"></div>
       <footer class="attribution">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
